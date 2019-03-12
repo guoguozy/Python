1398f5630e8635fa8eccb1df4e024fb04637fd22
guoguozy <893050234@qq.com>
2019-03-12 20:44:37 +0800

    add ps0 answer

---
+++ answer/ps0.py
@@ -0,0 +1,10 @@
+"""
+Author     : 郭梓煜  数据科学与计算机学院
+Student ID : 17341046
+mail       : 893050234@qq.com
+
+"""
+import math
+num1=input("Enter number x:")
+num2=input("Enter number y:")
+print("X**y = %s\nlog(x)=%s\n" % (int(num1)**int(num2),math.log(int(num1),2)))
